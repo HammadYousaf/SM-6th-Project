e022e899dea646ec9870a69879965464fc45efb3
HammadYousaf <hammadyousaf_sandhu@hotmail.com>
2015-04-26 21:33:43 -0700

    Synch

---
+++ Projectss/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Projectss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerIdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previousIdsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importIdListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportIdListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveIdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.capturesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recordingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.captureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.remoteOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disableConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionsToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.extrasToolStripMenuItem,
+            this.remoteOptionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(658, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registerIdToolStripMenuItem,
+            this.idToolStripMenuItem,
+            this.previousIdsToolStripMenuItem,
+            this.importIdListToolStripMenuItem,
+            this.exportIdListToolStripMenuItem,
+            this.saveIdToolStripMenuItem,
+            this.exitConnectionToolStripMenuItem});
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // registerIdToolStripMenuItem
+            // 
+            this.registerIdToolStripMenuItem.Name = "registerIdToolStripMenuItem";
+            this.registerIdToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.registerIdToolStripMenuItem.Text = "Register Id";
+            // 
+            // idToolStripMenuItem
+            // 
+            this.idToolStripMenuItem.Name = "idToolStripMenuItem";
+            this.idToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.idToolStripMenuItem.Text = "Login Id";
+            this.idToolStripMenuItem.Click += new System.EventHandler(this.idToolStripMenuItem_Click);
+            // 
+            // previousIdsToolStripMenuItem
+            // 
+            this.previousIdsToolStripMenuItem.Name = "previousIdsToolStripMenuItem";
+            this.previousIdsToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.previousIdsToolStripMenuItem.Text = "Previous Ids";
+            // 
+            // importIdListToolStripMenuItem
+            // 
+            this.importIdListToolStripMenuItem.Name = "importIdListToolStripMenuItem";
+            this.importIdListToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.importIdListToolStripMenuItem.Text = "Import Id list";
+            this.importIdListToolStripMenuItem.Click += new System.EventHandler(this.importIdListToolStripMenuItem_Click);
+            // 
+            // exportIdListToolStripMenuItem
+            // 
+            this.exportIdListToolStripMenuItem.Name = "exportIdListToolStripMenuItem";
+            this.exportIdListToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exportIdListToolStripMenuItem.Text = "Export Id list";
+            // 
+            // saveIdToolStripMenuItem
+            // 
+            this.saveIdToolStripMenuItem.Name = "saveIdToolStripMenuItem";
+            this.saveIdToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.saveIdToolStripMenuItem.Text = "Save Id";
+            // 
+            // exitConnectionToolStripMenuItem
+            // 
+            this.exitConnectionToolStripMenuItem.Name = "exitConnectionToolStripMenuItem";
+            this.exitConnectionToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exitConnectionToolStripMenuItem.Text = "Exit Connection";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.capturesToolStripMenuItem,
+            this.idListToolStripMenuItem,
+            this.recordingsToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // capturesToolStripMenuItem
+            // 
+            this.capturesToolStripMenuItem.Name = "capturesToolStripMenuItem";
+            this.capturesToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.capturesToolStripMenuItem.Text = "Captures";
+            // 
+            // idListToolStripMenuItem
+            // 
+            this.idListToolStripMenuItem.Name = "idListToolStripMenuItem";
+            this.idListToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.idListToolStripMenuItem.Text = "Id list";
+            // 
+            // recordingsToolStripMenuItem
+            // 
+            this.recordingsToolStripMenuItem.Name = "recordingsToolStripMenuItem";
+            this.recordingsToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.recordingsToolStripMenuItem.Text = "Recordings";
+            // 
+            // extrasToolStripMenuItem
+            // 
+            this.extrasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.captureToolStripMenuItem,
+            this.recordToolStripMenuItem});
+            this.extrasToolStripMenuItem.Name = "extrasToolStripMenuItem";
+            this.extrasToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.extrasToolStripMenuItem.Text = "Extras";
+            // 
+            // captureToolStripMenuItem
+            // 
+            this.captureToolStripMenuItem.Name = "captureToolStripMenuItem";
+            this.captureToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.captureToolStripMenuItem.Text = "Capture";
+            // 
+            // recordToolStripMenuItem
+            // 
+            this.recordToolStripMenuItem.Name = "recordToolStripMenuItem";
+            this.recordToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.recordToolStripMenuItem.Text = "Record";
+            // 
+            // remoteOptionsToolStripMenuItem
+            // 
+            this.remoteOptionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createConnectionToolStripMenuItem,
+            this.disableConnectionToolStripMenuItem});
+            this.remoteOptionsToolStripMenuItem.Name = "remoteOptionsToolStripMenuItem";
+            this.remoteOptionsToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.remoteOptionsToolStripMenuItem.Text = "Remote options";
+            // 
+            // createConnectionToolStripMenuItem
+            // 
+            this.createConnectionToolStripMenuItem.Name = "createConnectionToolStripMenuItem";
+            this.createConnectionToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.createConnectionToolStripMenuItem.Text = "Create Connection";
+            // 
+            // disableConnectionToolStripMenuItem
+            // 
+            this.disableConnectionToolStripMenuItem.Name = "disableConnectionToolStripMenuItem";
+            this.disableConnectionToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.disableConnectionToolStripMenuItem.Text = "Disable Connection";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Remote ID";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(143, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 153);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 436);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Remote Connect";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem remoteOptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem previousIdsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importIdListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportIdListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveIdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem capturesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recordingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem captureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disableConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerIdToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
